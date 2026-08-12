08ef540c92cecd96d9e38b0453667592ac697b37
Pablo Chacón <pachacon@espol.edu.ec>
2026-08-12 19:12:33 +0000

    Nuevo proyecto backend llamado analytics server

---
+++ dashboard/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+import requests
+from django.conf import settings
+
+
+def index(request):
+    response = requests.get(settings.API_URL)  # URL de la API
+    posts = response.json()  # Convertir la respuesta a JSON
+
+    # Número total de respuestas
+    total_responses = len(posts)
+
+    data = {
+        'title': "Landing Page' Dashboard",
+         'total_responses': total_responses,
+    }
+
+    return render(request, 'dashboard/index.html', data)
